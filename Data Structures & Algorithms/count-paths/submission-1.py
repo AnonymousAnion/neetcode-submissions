88c97d450d2fa6e62dc506c819baf57ef2854722
Liam Tomson-Moylan <liam.tomsonmoylan@gmail.com>
2026-03-31 22:56:02 -0500

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        prev_row = [1] * n
+
+        for r in range(m - 2, -1, -1):
+
+            row = [1] * n
+
+            for c in range(n - 2, -1, -1):
+
+                row[c] = row[c + 1] + prev_row[c]
+
+            prev_row = row
+
+        return prev_row[0]
