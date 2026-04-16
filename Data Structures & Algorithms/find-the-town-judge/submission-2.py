b8ea8c017d1baac460b13840bf8993a945ac27e4
Liam Tomson-Moylan <liam.tomsonmoylan@gmail.com>
2026-04-16 00:33:49 -0500

    Add: find-the-town-judge - submission-2

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-2.py
@@ -0,0 +1,39 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+
+        if not trust and n == 1:
+
+            return 1
+
+        trusting = dict()
+        trusted = dict()
+        people = set()
+
+        for t in trust:
+
+            people.add(t[0])
+            people.add(t[1])
+
+            if t[0] not in trusting:
+
+                trusting.update({t[0]: [t[1]]})
+
+            else:
+
+                trusting[t[0]].append(t[1])
+
+            if t[1] not in trusted:
+
+                trusted.update({t[1]: [t[0]]})
+
+            else:
+
+                trusted[t[1]].append(t[0])
+
+        for t in trusted:
+
+            if len(trusted[t]) == len(people) - 1 and t not in trusting:
+
+                return t
+
+        return -1
