8f0032c61cb7ec360c984715c144e1050d560d84
Liam Tomson-Moylan <liam.tomsonmoylan@gmail.com>
2026-04-22 23:32:22 -0500

    Add: search-for-word-ii - submission-4

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-4.py
@@ -0,0 +1,82 @@
+class TrieNode:
+
+    def __init__(self, word: str = "", children: Dict = None) -> None:
+
+        self.children = {}
+        self.word = word
+
+    def add_word(self, word: str) -> None:
+
+        current = self
+
+        for char in word:
+
+            if char not in current.children:
+
+                current.children.update({char: TrieNode()})
+
+            current = current.children[char]
+
+        current.word = word
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+
+        ROWS = len(board)
+        COLS = len(board[0])
+
+        present_words = set()
+        
+        # Form a Trie from words
+        trie = TrieNode()
+
+        for word in words:
+
+            trie.add_word(word)
+
+        visited = set()
+        dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+        def dfs(node: TrieNode, cell: (int, int)) -> None:
+
+            visited.add((cell[0], cell[1]))
+            row = cell[0]
+            col = cell[1]
+
+            char = board[row][col]
+
+            if char in node.children:
+
+                next_node = node.children[char]
+
+                if next_node.word:
+
+                    present_words.add(next_node.word)
+
+                for d in dirs:
+
+                    r = row + d[0]
+                    c = col + d[1]
+
+                    if min(r, c) >= 0 and r < ROWS and c < COLS: # valid Node
+
+                        new_cell = (r, c)
+
+                        if new_cell not in visited:
+                        
+                            dfs(next_node, new_cell)
+
+            visited.remove((cell[0], cell[1]))
+
+        # Loop through each cell and attempt
+        # to form words out of neighbors in a DFS manner
+        # at each one.
+        for i in range(ROWS):
+
+            for j in range(COLS):
+
+                cell = (i, j)
+
+                dfs(trie, cell)
+
+        return list(present_words)
