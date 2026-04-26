874568d583c059fc48089d69fd3f02ed7fb3682f
Liam Tomson-Moylan <liam.tomsonmoylan@gmail.com>
2026-04-26 12:57:47 -0500

    Add: permutations-ii - submission-4

---
+++ Data Structures & Algorithms/permutations-ii/submission-4.py
@@ -0,0 +1,27 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        
+        permutations = []
+        current = []
+        freqs = Counter(nums)
+
+        def dfs() -> None:
+
+            if len(nums) == len(current):
+
+                permutations.append(current.copy())
+                return # Early termination
+
+            for num in freqs.keys():
+
+                if freqs[num] > 0:
+
+                    current.append(num)
+                    freqs[num] -= 1
+                    dfs()
+                    current.pop()
+                    freqs[num] += 1
+
+        dfs()
+
+        return permutations
