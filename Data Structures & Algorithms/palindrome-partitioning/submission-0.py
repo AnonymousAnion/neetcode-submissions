05116faeaaeabbbaec95031bc286508c718d79e5
Liam Tomson-Moylan <liam.tomsonmoylan@gmail.com>
2026-04-29 18:11:00 -0500

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.py
@@ -0,0 +1,71 @@
+class Solution:
+
+    def is_palindrome(self, s: str) -> bool:
+
+        i = 0
+
+        while i < len(s) // 2:
+
+            if s[i] != s[len(s) - 1 - i]:
+
+                return False
+
+            i += 1
+
+        return True
+
+    def partition(self, s: str) -> List[List[str]]:
+        
+        cur_subs = [s[0]]
+        cur_pals = [True]
+
+        palindromic_substrings = []
+
+        def substring_builder(i: int) -> None:
+
+            nonlocal cur_subs
+            nonlocal cur_pals
+
+            if i == len(s):
+
+                all_pals = True
+
+                for pal in cur_pals:
+
+                    if not pal:
+
+                        all_pals = False
+                        break
+
+                if all_pals:
+
+                    palindromic_substrings.append(cur_subs.copy())
+
+                return
+
+            char = s[i]
+
+            # Case 1: Add character to last substring.
+            # This should happen regardless of whether
+            # the last substring is a palindrome because
+            # it may become a palindrome later.
+            cur_subs[-1] += char
+            cur_pals[-1] = self.is_palindrome(cur_subs[-1])
+            substring_builder(i + 1)
+            cur_subs[-1] = cur_subs[-1][:len(cur_subs[-1]) - 1]
+            cur_pals[-1] = self.is_palindrome(cur_subs[-1])
+
+            # Case 2: Add character as a new substring.
+            # This should only occur if the last substring
+            # is a palindrome.
+            if cur_pals[-1]:
+
+                cur_subs.append(char)
+                cur_pals.append(True)
+                substring_builder(i + 1)
+                cur_subs.pop()
+                cur_pals.pop()
+
+        substring_builder(1)
+
+        return palindromic_substrings
