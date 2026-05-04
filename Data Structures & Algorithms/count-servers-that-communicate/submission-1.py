2d230824205ce2d9714e4d59fcc1fa4e0b4e770e
Liam Tomson-Moylan <liam.tomsonmoylan@gmail.com>
2026-05-04 14:27:13 -0500

    Add: count-servers-that-communicate - submission-1

---
+++ Data Structures & Algorithms/count-servers-that-communicate/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def countServers(self, grid: List[List[int]]) -> int:
+        
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        row_counts = [0] * ROWS
+        col_counts = [0] * COLS
+        count = 0
+
+        for r in range(ROWS):
+
+            for c in range(COLS):
+
+                if grid[r][c]:
+
+                    row_counts[r] += 1
+                    col_counts[c] += 1
+
+        for r in range(ROWS):
+
+            for c in range(COLS):
+
+                if grid[r][c]:
+                    
+                    if row_counts[r] > 1 or col_counts[c] > 1:
+
+                        count += 1
+
+        return count
