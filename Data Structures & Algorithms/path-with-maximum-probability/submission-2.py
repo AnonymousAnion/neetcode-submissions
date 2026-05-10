46fb37df05d564c8f040118d01bc88692ddc772e
Liam Tomson-Moylan <liam.tomsonmoylan@gmail.com>
2026-05-10 13:35:44 -0500

    Add: path-with-maximum-probability - submission-2

---
+++ Data Structures & Algorithms/path-with-maximum-probability/submission-2.py
@@ -0,0 +1,54 @@
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        
+        probabilities: Dict[int, List[Tuple[int, float]]] = dict()
+        visited = set()
+
+        for i in range(len(edges)):
+
+            edge = edges[i]
+            prob = succProb[i]
+
+            if edge[0] not in probabilities:
+
+                probabilities.update({edge[0]: [(edge[1], prob)]})
+
+            else:
+
+                probabilities[edge[0]].append((edge[1], prob))
+
+            if edge[1] not in probabilities:
+
+                probabilities.update({edge[1]: [(edge[0], prob)]})
+
+            else:
+
+                probabilities[edge[1]].append((edge[0], prob))
+
+        priority_queue = [(1.0, start_node)]
+        max_probability = 0
+
+        while priority_queue:
+
+            probability, node = heapq.heappop_max(priority_queue)
+
+            if node in visited:
+
+                continue
+
+            visited.add(node)
+
+            if node == end_node:
+
+                max_probability = max(max_probability, probability)
+
+            if node in probabilities:
+
+                for neighbor in probabilities[node]:
+
+                    if neighbor not in visited:
+
+                        heapq.heappush_max(priority_queue, (probability * neighbor[1], neighbor[0]))
+        
+        return max_probability
+        
