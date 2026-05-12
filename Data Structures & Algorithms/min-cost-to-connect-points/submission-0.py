c1630794b9d96883b8038fec9afc000580ddc731
Liam Tomson-Moylan <liam.tomsonmoylan@gmail.com>
2026-05-12 17:00:38 -0500

    Add: min-cost-to-connect-points - submission-0

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-0.py
@@ -0,0 +1,48 @@
+class Solution:
+
+    def manhattan_dist(self, p1: List[int], p2: List[int]) -> int:
+
+        return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+
+        visited = set()
+        distances = defaultdict(list)
+        
+        for i in range(len(points)):
+
+            p1 = points[i]
+
+            for j in range(i + 1, len(points)):
+
+                p2 = points[j]
+                distances[(p1[0], p1[1])].append((self.manhattan_dist(p1, p2), (p2[0], p2[1])))
+                distances[(p2[0], p2[1])].append((self.manhattan_dist(p1, p2), (p1[0], p1[1])))
+
+        # print(distances)
+
+        cost = 0
+        src = (points[0][0], points[0][1])
+        pq = [(0, src)]
+        
+        while pq:
+
+            dist, src = heapq.heappop(pq)
+            # print("src: ", src)
+
+            if src in visited:
+
+                continue
+
+            cost += dist
+            visited.add(src)
+
+            for dist, dst in distances[src]:
+
+                if dst not in visited:
+
+                    # print("dist: ", dist)
+                    # print("dst: ", dst)
+                    heapq.heappush(pq, (dist, dst))
+
+        return cost
