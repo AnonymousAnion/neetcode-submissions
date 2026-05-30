0e7386aab27a2272d8349a57d35ce9bbec033aa3
Liam Tomson-Moylan <liam.tomsonmoylan@gmail.com>
2026-05-30 16:16:37 -0500

    Add: closest-binary-search-tree-value - submission-0

---
+++ Data Structures & Algorithms/closest-binary-search-tree-value/submission-0.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+
+        closest = root.val
+        
+        def binary_search(node: Optional[TreeNode]) -> None:
+
+            if not node:
+
+                return
+
+            nonlocal closest
+            nonlocal target
+
+            if abs(node.val - target) < abs(closest - target):
+
+                closest = node.val
+
+            if target < node.val:
+
+                binary_search(node.left)
+
+            elif target > node.val:
+
+                binary_search(node.right)
+
+        binary_search(root)
+
+        return closest
